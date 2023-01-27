14572421c371e69aa0f1cf7ee3b49214a4262b31
otavioescalnte <otavioescalantedev@outlook.com>
2023-01-27 01:09:26 -0300

    project fixed and README.md added

---
+++ README.md
@@ -0,0 +1,57 @@
+# DT Money
+
+## Projeto para controle de finanças Desenvolvido em React com Typescript durante a trilha ignite da Rocketseat ![image 10 (1)](https://user-images.githubusercontent.com/77994942/205470744-bd9734ff-559c-4bb5-9bab-60d4eb5c69fd.svg)
+
+### Funcionalidades do projeto
+<ul>
+<li>Cadastrar entrada de transações</li>
+<li>Cadastrar saída de transações</li>
+<li>Consultar transações</li>
+</ul>
+
+![Screenshot_20230520_233906](https://github.com/OtavioEscalnte/02-ignite-timer/assets/77994942/effccf41-3664-4462-a5ed-c5df65e8ff0c)
+
+### Bibliotecas
+<ul>
+<li>React Hook Form</li>
+<li>Styled Components</li>
+<li>phosphor-icons</li>
+<li>Radix UI</li>
+<li>zod</li>
+<li>Axios</li>
+</ul>
+
+### Como rodar o projeto?
+
+<p>Abra seu terminal copie e cole o código abaixo.</p>
+
+```bash
+git clone https://github.com/OtavioEscalnte/dt-money
+
+```
+<p>Para entrar no repositório do projeto copie e cole o código abaxio.</p>
+
+```bash
+cd dt-money
+
+
+```
+<p>Para instalar as dependências do projeto copie e cole o código abaxio.</p>
+
+```bash
+yarn ou npm i
+
+```
+<p>Para rodar o projeto copie e cole o código abaixo.</p>
+
+```bash
+yarn dev ou npm run dev
+
+```
+
+<p>Para iniciar o servidor execute o seguinte comando no terminal</p>
+
+```bash
+npx json-server server.json
+
+```
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,59 +1,50 @@
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
-import { SearchForm } from "./components/SearchForm";
-import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import { useContextSelector } from 'use-context-selector'
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { TransactionsContext } from '../../context/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
+import { SearchForm } from './components/SearchForm'
+
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
+
   return (
     <div>
-      <Header/>
-      <Summary/>
+      <Header />
+      <Summary />
 
-    <TransactionsContainer>
-    <SearchForm/>
+      <TransactionsContainer>
+        <SearchForm />
 
-      <TransactionsTable>
-        <tbody>
-          <tr>
-            <td width='50%'>Desenvolvimento de site</td>
-            <td>
-            <PriceHighlight variant='income'>R$ 12.000,00</PriceHighlight>
-            </td>
-            <td>Venda</td>
-            <td>12/04/2023</td>
-          </tr>
-          <tr>
-            <td width='50%'>Hamburguer</td>
-            <td>
-            <PriceHighlight variant="outcome">
-              - R$ 59,00
-            </PriceHighlight>
-            </td>
-            <td>Alimentação</td>
-            <td>10/04/2023</td>
-          </tr>
-          <tr>
-            <td width='50%'>Aluguel do apartamento</td>
-            <td>
-              <PriceHighlight variant="outcome">
-                - R$ 1.200,00
-                </PriceHighlight>
-                </td>
-            <td>Casa</td>
-            <td>27/03/2023</td>
-          </tr>
-          <tr>
-            <td width='50%'>Computador</td>
-            <td>
-              <PriceHighlight variant="income">
-              R$ 5.400,00
-              </PriceHighlight>
-              </td>
-            <td>Venda</td>
-            <td>15/03/2023</td>
-          </tr>
-        </tbody>
-      </TransactionsTable>
+        <TransactionsTable>
+          <tbody>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </TransactionsTable>
       </TransactionsContainer>
     </div>
   )
